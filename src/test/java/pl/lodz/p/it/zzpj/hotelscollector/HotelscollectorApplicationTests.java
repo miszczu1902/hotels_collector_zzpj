02b85ac29d81bcfa803d89f68bf7ac2f047e361b
228932 Malinowska Magdalena <228932@edu.p.lodz.pl>
2023-03-31 14:58:00 +0200

    Add example of simple test

--- src/test/java/pl/lodz/p/it/zzpj/hotelscollector/HotelscollectorApplicationTests.java
+++ src/test/java/pl/lodz/p/it/zzpj/hotelscollector/HotelscollectorApplicationTests.java
@@ -3,8 +3,18 @@ package pl.lodz.p.it.zzpj.hotelscollector;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 @SpringBootTest
 class HotelscollectorApplicationTests {
+
+	@Test
+	void shouldTestExampleMethod() {
+		SumCalculator sumCalculator = new SumCalculator();
+		assertThat(sumCalculator.sumCalculator(1,5)).isEqualTo(6);
+
+	}
 
 	@Test
 	void contextLoads() {
