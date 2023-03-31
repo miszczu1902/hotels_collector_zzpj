59195b3f814fbd8f8f0bc2d25686c99cbdc5863f
236605 Miszczak Bartosz <236605@edu.p.lodz.pl>
2023-03-31 11:48:59 +0200

    Initial commit

---
+++ src/main/java/pl/lodz/p/it/zzpj/hotelscollector/HOTELSCOLLETOR/HotelscolletorApplication.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.it.zzpj.hotelscollector.HOTELSCOLLETOR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelscolletorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelscolletorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/lodz/p/it/zzpj/hotelscollector/HOTELSCOLLETOR/HotelscolletorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.it.zzpj.hotelscollector.HOTELSCOLLETOR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelscolletorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
